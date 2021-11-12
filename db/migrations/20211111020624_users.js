ffcc51fe60549efb0b2d1ce1b4fddabaedf93867
Pedro A. González <pgonzalez@post.com>
2021-11-11 20:59:21 -0500

    Configurar migraciones con propiedades de tabla + agregar script migrate

--- db/migrations/20211111020624_users.js
+++ db/migrations/20211111020624_users.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+  // CREAR TABLA
+  return knex.schema.createTable('users', function(table) {
+    table
+      .string('userID', 6)
+      .notNullable()
+      .unique()
+      .primary();
+    table
+      .string('email', 50)
+      .notNullable()
+      .unique();
+    table.string('password', 50).notNullable();
+    table
+      .boolean('isActive')
+      .notNullable()
+      .defaultTo(true);
+  });
 };
 
 exports.down = function(knex) {
-  
+  // ELMINAR TABLA
+  return knex.schema.dropTableIfExists('users');
 };
--- db/migrations/20211111020633_urls.js
+++ db/migrations/20211111020633_urls.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex) {
-  
+  // CREAR TABLA
+  return knex.schema.createTable('urls', function(table) {
+    table
+      .string('urlID', 6)
+      .notNullable()
+      .unique()
+      .primary();
+    table
+      .string('longURL', 6)
+      .notNullable()
+      .unique();
+    table.string('userID', 6).notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-  
+  // ELMINAR TABLA
+  return knex.schema.dropTableIfExists('urls');
 };
