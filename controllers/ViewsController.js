9784f803629cff3a4c4ca1c73ffae39ed4ec28fc
Pedro A. González <pgonzalez@post.com>
2021-11-23 22:38:53 -0500

    Condicionar y estilizar barra de navegación/header + agregar dummy user

--- controllers/ViewsController.js
+++ controllers/ViewsController.js
@@ -1,13 +1,16 @@
 const home = (req, res) => {
-  return res.render('index');
+  const templateVars = { user: null }; // Usuario sin sesión
+  return res.render('index', templateVars);
 };
 
 const register = (req, res) => {
-  return res.render('register');
+  const templateVars = { user: null }; // Usuario sin sesión
+  return res.render('register', templateVars);
 };
 
 const login = (req, res) => {
-  return res.render('login');
+  const templateVars = { user: null }; // Usuario sin sesión
+  return res.render('login', templateVars);
 };
 
 const myUrls = (req, res) => {
@@ -20,17 +23,40 @@ const myUrls = (req, res) => {
     { urlID: '038we4', longURL: 'http://facebook.com' },
     { urlID: 'g4349g', longURL: 'http://twitter.com' }
   ];
-  const templateVars = { urls };
+
+  const user = {
+    email: 'user@example.com',
+    password: 'Passw0rd123!'
+  };
+
+  const templateVars = { urls, user }; // Usuario con inicio de sesión
+  // const templateVars = { urls, user: null }; // Usuario sin sesión
 
   return res.render('my-urls', templateVars);
 };
 
 const newUrl = (req, res) => {
-  return res.render('new-url');
+  const user = {
+    email: 'user@example.com',
+    password: 'Passw0rd123!'
+  };
+
+  const templateVars = { user }; // Usuario con inicio de sesión
+  // const templateVars = { urls, user: null }; // Usuario sin sesión
+  //
+  return res.render('new-url', templateVars);
 };
 
 const showUrl = (req, res) => {
-  return res.render('url');
+  const user = {
+    email: 'user@example.com',
+    password: 'Passw0rd123!'
+  };
+
+  const templateVars = { user }; // Usuario con inicio de sesión
+  // const templateVars = { urls, user: null }; // Usuario sin sesión
+  //
+  return res.render('url', templateVars);
 };
 
 const redirectUrl = (req, res) => {
