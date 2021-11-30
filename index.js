2d85eead0d3dad81f02ef9144beda5f03ce04ebd
Pedro A. González <pgonzalez@post.com>
2021-11-29 22:29:39 -0500

    Refactorizar server en app + index

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
-// MODULES
 require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const { errors } = require('celebrate');
-const cookieSession = require('cookie-session');
-const methodOverride = require('method-override');
-
-const app = express();
+const app = require('./app');
 const PORT = process.env.PORT || 8080;
-
-const routes = require('./routes');
-const db = require('./db');
-
-// MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
-app.use(
-  cookieSession({
-    name: 'session',
-    keys: ['tinyclone', 'Code', 'G7'],
-
-    // Cookie Options
-    maxAge: 10 * 60 * 1000 // Se recomienda entre 10-15 min de duración para evitar vulnerabilidades
-  })
-);
-
-app.use(routes);
-app.use(errors());
 
 // LISTENER
 app.listen(PORT, () =>
